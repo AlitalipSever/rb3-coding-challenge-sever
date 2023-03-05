e4a87787346d15d6768e07650815f90299c94eb9
AlitalipSever <alitalipsever@gmail.com>
2023-03-05 11:50:26 +0100

    task-005 feat: sortData func added

--- src/components/AwesomeTable/index.tsx
+++ src/components/AwesomeTable/index.tsx
@@ -1,6 +1,16 @@
-import {FC, useState} from 'react';
+import {FC, useCallback, useState} from 'react';
 import {ArrayOfSerializedData, SerializedData} from '../../utils/serializeData';
-import {Container, Paper, Table, TableBody, TableCell, TableHead, TableRow} from '@mui/material';
+import {
+  Container,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 
 type Props = {
   serializedData: ArrayOfSerializedData;
@@ -11,6 +21,24 @@ type Props = {
 type SortKeys = 'name' | 'birthYear' | 'eyeColor' | 'homeworldName';
 type SortOrder = 'asc' | 'desc';
 
+function sortData({
+  tableData,
+  sortKey,
+  reverse,
+}: {
+  tableData: ArrayOfSerializedData;
+  sortKey: SortKeys;
+  reverse: boolean;
+}) {
+  if (!sortKey) return tableData;
+  const sortedData = tableData.sort((a, b) => {
+    return a[sortKey] > b[sortKey] ? 1 : -1;
+  });
+
+  if (reverse) sortedData.reverse();
+  return sortedData;
+}
+
 const AwesomeTable: FC<Props> = ({serializedData, itemsPerPage, currentPage}) => {
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -18,8 +46,22 @@ const AwesomeTable: FC<Props> = ({serializedData, itemsPerPage, currentPage}) =>
   const [sortKey, setSortKey] = useState<SortKeys>('birthYear');
   const [sortOrder, setSortOrder] = useState<SortOrder>('asc');
 
+  const sortedData = useCallback(
+    () => sortData({tableData, sortKey, reverse: sortOrder === 'desc'}),
+    [tableData, sortKey, sortOrder],
+  );
+
+  const handleSortClick = (key: SortKeys) => {
+    if (key === sortKey) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortKey(key);
+      setSortOrder('asc');
+    }
+  };
+
   const AvesomeTableBody = () => {
-    return tableData.map((n: SerializedData) => (
+    return sortedData().map((n: SerializedData) => (
       <TableRow key={n.id}>
         <TableCell>{n.name}</TableCell>
         <TableCell>{n.birthYear}</TableCell>
@@ -43,7 +85,22 @@ const AwesomeTable: FC<Props> = ({serializedData, itemsPerPage, currentPage}) =>
           <TableHead>
             <TableRow>
               {headers.map(n => (
-                <TableCell key={n.key}>{n.label}</TableCell>
+                <TableCell
+                  key={n.key}
+                  onClick={() =>
+                    handleSortClick(n.key as 'name' | 'birthYear' | 'eyeColor' | 'homeworldName')
+                  }>
+                  {n.label}
+                  {sortKey === n.key && (
+                    <IconButton size="small">
+                      {sortOrder === 'asc' ? (
+                        <Typography>foo</Typography>
+                      ) : (
+                        <Typography>zoo</Typography>
+                      )}
+                    </IconButton>
+                  )}
+                </TableCell>
               ))}
             </TableRow>
           </TableHead>
