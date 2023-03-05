a3dc78a779bae7487ebaec1dba47d8e55d6836ac
AlitalipSever <alitalipsever@gmail.com>
2023-03-05 11:19:20 +0100

    task-005 feat: table rows and headers added

--- src/components/AwesomeTable/index.tsx
+++ src/components/AwesomeTable/index.tsx
@@ -1,9 +1,48 @@
 import {FC} from 'react';
+import {ArrayOfSerializedData, SerializedData} from '../../utils/serializeData';
+import {Container, Paper, Table, TableBody, TableCell, TableHead, TableRow} from '@mui/material';
 
-type Props = {};
+type Props = {
+  serializedData: ArrayOfSerializedData;
+  currentPage: number;
+  itemsPerPage: number;
+};
 
-const AwesomeTable: FC<Props> = ({}) => {
-  return <div>AwesomeTable</div>;
+const AwesomeTable: FC<Props> = ({serializedData, itemsPerPage, currentPage}) => {
+  const AvesomeTableBody = () => {
+    return serializedData.map((n: SerializedData) => (
+      <TableRow key={n.id}>
+        <TableCell>{n.name}</TableCell>
+        <TableCell>{n.birthYear}</TableCell>
+        <TableCell>{n.eyeColor}</TableCell>
+        <TableCell>{n.homeworldName}</TableCell>
+      </TableRow>
+    ));
+  };
+
+  const headers = [
+    {key: 'name', label: 'Name'},
+    {key: 'birthYear', label: 'Birth Year'},
+    {key: 'eyeColor', label: 'Eye Color'},
+    {key: 'homeworldName', label: 'Homeworld'},
+  ];
+
+  return (
+    <Container>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {headers.map(n => (
+                <TableCell key={n.key}>{n.label}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody> {AvesomeTableBody()}</TableBody>
+        </Table>
+      </Paper>
+    </Container>
+  );
 };
 
 export default AwesomeTable;
