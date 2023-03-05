cdf849fdf8312b9ea1265c85facfd40f1beb1141
AlitalipSever <alitalipsever@gmail.com>
2023-03-05 11:18:46 +0100

    task-005 feat: fetchPage added

--- src/pages/Manual/index.tsx
+++ src/pages/Manual/index.tsx
@@ -1,16 +1,32 @@
-import {FC, useState} from 'react';
+import {FC, useEffect, useState} from 'react';
 import {Container} from '@mui/material';
 import useGetData from '../../hooks/useGetData';
-import {ArrayOfSerializedData} from '../../utils/serializeData';
+import {ArrayOfSerializedData, serializeData} from '../../utils/serializeData';
 import AwesomeTable from '../../components/AwesomeTable';
 
+const ITEMS_PER_PAGE = 10;
 type Props = {};
 const ManualPage: FC<Props> = ({}) => {
   const {fetchPage, data, loading} = useGetData();
   const [serializedData, setSerializedData] = useState<ArrayOfSerializedData>([]);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    fetchPage();
+    if (data) {
+      const serialized = serializeData(data.allPeople.edges);
+      setSerializedData(serialized);
+      setTotalPages(Math.ceil(serialized.length / ITEMS_PER_PAGE));
+    }
+  }, [data]);
+
   return (
     <Container>
-      <AwesomeTable />
+      <AwesomeTable
+        serializedData={serializedData}
+        itemsPerPage={ITEMS_PER_PAGE}
+        currentPage={totalPages}
+      />
     </Container>
   );
 };
