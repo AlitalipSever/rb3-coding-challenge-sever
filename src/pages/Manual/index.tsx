12c80f2fe1fb52813a6675a873a7edd7dbdedb7f
AlitalipSever <alitalipsever@gmail.com>
2023-03-05 11:28:40 +0100

    task-005 feat: pagination added

--- src/pages/Manual/index.tsx
+++ src/pages/Manual/index.tsx
@@ -1,8 +1,9 @@
 import {FC, useEffect, useState} from 'react';
-import {Container} from '@mui/material';
+import {Container, Pagination, Typography} from '@mui/material';
 import useGetData from '../../hooks/useGetData';
 import {ArrayOfSerializedData, serializeData} from '../../utils/serializeData';
 import AwesomeTable from '../../components/AwesomeTable';
+import Box from '@mui/material/Box';
 
 const ITEMS_PER_PAGE = 10;
 type Props = {};
@@ -10,6 +11,7 @@ const ManualPage: FC<Props> = ({}) => {
   const {fetchPage, data, loading} = useGetData();
   const [serializedData, setSerializedData] = useState<ArrayOfSerializedData>([]);
   const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     fetchPage();
@@ -22,11 +24,14 @@ const ManualPage: FC<Props> = ({}) => {
 
   return (
     <Container>
+      <Typography color={'black'}>Page: {currentPage}</Typography>
+      <Box>{loading && <div>Loading...</div>}</Box>
       <AwesomeTable
         serializedData={serializedData}
         itemsPerPage={ITEMS_PER_PAGE}
         currentPage={totalPages}
       />
+      <Pagination count={totalPages} page={currentPage} />
     </Container>
   );
 };
